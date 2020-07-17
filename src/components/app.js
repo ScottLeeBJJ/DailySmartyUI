6ce3904b551e913fa2b1c5237433ae138e0bf61c
ScottLeeBJJ <scottlee5455@outlook.com>
2020-07-17 15:17:01 -0700

    added components

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Logo from '../components/logo';
+import SearchBar from '../components/searchBar';
+import RecentPost from './recentPost';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>Logo</div>
+        <Logo />
+        <SearchBar />
+        <RecentPost />
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="Logo-main">
+                <img alt="logo" src="/assets/ds_circle_logo.png" />
+            </div>
+        )
+    }
+}
+
+export default Logo;
